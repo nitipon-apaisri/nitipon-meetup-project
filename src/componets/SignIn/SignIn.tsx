1b8a5b739d418e851e067297244be55a2aa16892
nitipon apaisri <nitipon.apaisri@gmail.com>
2021-12-18 23:29:15 +0100

    Added style for sign in page

--- src/componets/SignIn/SignIn.tsx
+++ src/componets/SignIn/SignIn.tsx
@@ -2,6 +2,7 @@ import { useState, useContext } from "react";
 import { useNavigate } from "react-router";
 import { users } from "../../db/users";
 import { AuthContext } from "../../store/authContext";
+import illus from "../../assets/img/illus.svg";
 const SignIn = () => {
     const authContext = useContext(AuthContext);
     const [username, setUsername] = useState("");
@@ -26,31 +27,42 @@ const SignIn = () => {
     };
     return (
         <div className="user-auth-container">
-            <div className="title">
-                <h1>Sign In</h1>
-            </div>
-            <div className="inputs">
-                <div className="username">
-                    <label htmlFor="username">Username</label>
-                    <input
-                        type="text"
-                        onChange={(e) => {
-                            setUsername(e.target.value);
-                        }}
-                    />
+            <div className="auth-contents card">
+                <div className="left">
+                    <img src={illus} alt="illus" />
+                    <h1>Make the world a better Place</h1>
                 </div>
-                <div className="password">
-                    <label htmlFor="password">Password</label>
-                    <input
-                        type="password"
-                        onChange={(e) => {
-                            setPasswprd(e.target.value);
-                        }}
-                    />
+                <div className="right">
+                    <div className="title">
+                        <h1>Sign In</h1>
+                    </div>
+                    <hr />
+                    <div className="inputs">
+                        <div className="username">
+                            <label htmlFor="username">Username</label>
+                            <input
+                                type="text"
+                                onChange={(e) => {
+                                    setUsername(e.target.value);
+                                }}
+                            />
+                        </div>
+                        <div className="password">
+                            <label htmlFor="password">Password</label>
+                            <input
+                                type="password"
+                                onChange={(e) => {
+                                    setPasswprd(e.target.value);
+                                }}
+                            />
+                        </div>
+                        <div className="footer">
+                            <button onClick={submit}>
+                                <p>Sign In</p>
+                            </button>
+                        </div>
+                    </div>
                 </div>
-                <button onClick={submit}>
-                    <p>Sign In</p>
-                </button>
             </div>
         </div>
     );
